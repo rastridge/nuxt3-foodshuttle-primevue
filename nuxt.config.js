7eaa60d36b4b3d1c190297c3293a70a7c26e833b
rastridge <rfa@me.com>
2023-03-02 14:56:47 -0500

    new environ variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,8 +58,10 @@ export default defineNuxtConfig({
 		public: {
 			CLOUD_NAME: process.env.CLOUD_NAME,
 			CLOUD_API_KEY: process.env.CLOUD_API_KEY,
+			CLOUD_API: process.env.CLOUD_API,
 			CLOUD_API_SECRET: process.env.CLOUD_API_SECRET,
 			CLOUD_UPLOAD_PRESET: process.env.CLOUD_UPLOAD_PRESET,
+			MEDIA_API: process.env.MEDIA_API,
 		},
 	},
 
